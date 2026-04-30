4c0c9337183d8dcae4be2eb434a83953384d97d5
ia901092 <ia901092@gmail.com>
2026-04-30 08:55:21 +0000

    Skapa dropdown med tillverkare och  Anrop till fordonstjänsten med valt land  steg 3 och 4

--- xml/index.php
+++ xml/index.php
@@ -47,3 +47,44 @@ print_r($manufacturers);
 
 </body>
 </html>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Vehicles</title>
+</head>
+<body>
+
+<?php
+$selected = "";
+if (isset($_GET['country'])) {
+    $selected = $_GET['country'];
+}
+
+$jsontext = file_get_contents("https://wwwlab.webug.se/examples/XML/vehiclesservice/manufacturer");
+$manufacturers = json_decode($jsontext, true);
+?>
+
+<form action="" method="get">
+    <select name="country">
+        <option value="">-- Select --</option>
+<?php
+foreach ($manufacturers as $m) {
+    echo "<option value=\"" . $m[1] . "\">" . $m[0] . "</option>";
+}
+?>
+    </select>
+    <input type="submit" value="Show vehicles">
+</form>
+
+</body>
+</html>
+<?php
+if ($selected != "") {
+    $url = "https://wwwlab.webug.se/examples/XML/vehiclesservice/vehicles/?country=" . $selected;
+    $jsontext = file_get_contents($url);
+    $data = json_decode($jsontext, true);
+
+    print_r($data);
+}
+?>
