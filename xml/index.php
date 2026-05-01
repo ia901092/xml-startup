e8eade3119dfca8f1d814149af50f5327fcce192
ia901092 <ia901092@gmail.com>
2026-05-01 07:29:59 +0000

    Lägg till nästlade tabeller för tillverkare och modeller Visa fordonsbild, modell, typ, hästkrafter och årsmodell Färgmarkera hästkrafter över 300 HP i rött Förbättra CSS-styling för tabeller och layout sista steget  klar sista steget

--- xml/index.php
+++ xml/index.php
@@ -3,55 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <title>Vehicles</title>
-</head>
-<body>
-
-<?php
-$selected = "";
-if (isset($_GET['country'])) {
-    $selected = $_GET['country'];
-}
-?>
-
-<form action="" method="get">
-    <input type="text" name="country">
-    <input type="submit" value="Show vehicles">
-</form>
-
-</body>
-</html>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Vehicles</title>
-</head>
-<body>
-
-<?php
-$selected = "";
-if (isset($_GET['country'])) {
-    $selected = $_GET['country'];
-}
-
-$jsontext = file_get_contents("https://wwwlab.webug.se/examples/XML/vehiclesservice/manufacturer");
-$manufacturers = json_decode($jsontext, true);
-
-print_r($manufacturers);
-?>
-
-<form action="" method="get">
-    <input type="text" name="country">
-    <input type="submit" value="Show vehicles">
-</form>
-
-</body>
-</html>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Vehicles</title>
+    <style>
+        body {
+            font-family: Arial;
+        }
+        table {
+            border: 1px solid black;
+            border-collapse: collapse;
+            margin-bottom: 10px;
+        }
+        td, th {
+            border: 1px solid black;
+            padding: 5px;
+        }
+        th {
+            background-color: #bbdefb;
+        }
+        tr:nth-child(even) {
+            background-color: #f0f0f0;
+        }
+        img {
+            width: 80px;
+        }
+        .red {
+            color: red;
+        }
+    </style>
 </head>
 <body>
 
@@ -77,14 +54,43 @@ foreach ($manufacturers as $m) {
     <input type="submit" value="Show vehicles">
 </form>
 
-</body>
-</html>
 <?php
 if ($selected != "") {
     $url = "https://wwwlab.webug.se/examples/XML/vehiclesservice/vehicles/?country=" . $selected;
     $jsontext = file_get_contents($url);
     $data = json_decode($jsontext, true);
 
-    print_r($data);
+    foreach ($data as $man) {
+        echo "<table>";
+        echo "<tr><td>" . $man[0] . "</td><td>" . $selected . "</td></tr>";
+        echo "<tr><td colspan=\"2\">";
+
+        echo "<table>";
+        echo "<tr><th>Image</th><th>Model</th><th>Type</th><th>HP</th><th>Year</th></tr>";
+
+        foreach ($man[1] as $v) {
+            $hp = $v[2];
+            if (intval(substr($hp, 0, -2)) > 300) {
+                $color = "red";
+            } else {
+                $color = "black";
+            }
+
+            echo "<tr>";
+            echo "<td><img src=\"https://wwwlab.webug.se/examples/XML/vehicleImages/" . $v[4] . "\"></td>";
+            echo "<td>" . $v[0] . "</td>";
+            echo "<td>" . $v[1] . "</td>";
+            echo "<td style=\"color:" . $color . "\">" . $v[2] . "</td>";
+            echo "<td>" . $v[3] . "</td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+        echo "</td></tr>";
+        echo "</table>";
+    }
 }
 ?>
+
+</body>
+</html>
